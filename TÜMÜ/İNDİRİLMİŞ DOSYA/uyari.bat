fb6f86b13b0ca30e23ae3a5f2d7c396e338305cf
eymenegetekiner-alt <eymenegetekiner@gmail.com>
2026-06-25 17:37:57 +0300

    Update uyari.bat

--- TÜMÜ/İNDİRİLMİŞ DOSYA/uyari.bat
+++ TÜMÜ/İNDİRİLMİŞ DOSYA/uyari.bat
@@ -5,6 +5,21 @@ echo     UYARI MESAJI
 echo =======================
 echo.
 echo Bu sadece bir test dosyasi
-echo Bilgisayar guvende :)
-echo.
+echo Bilgisayar guvende olmayabilir
+echo bilgisayar tehlikede
+color a
+color rd
+color 1
+color 2
+color 3
+echo 20 saniye icinde bilgisayarında kamera acilicak
+timeout 20
+start microsoft.windows.camera:
+start
+@echo off
+color 4
+echo you
+timeout 2
+echo died
 pause
+exit
