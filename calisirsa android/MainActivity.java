dbff0aa689d648ea8c90f35238c1dc2713a92d2a
eymenegetekiner-alt <eymenegetekiner@gmail.com>
2026-06-25 18:15:43 +0300

    Add files via upload

---
+++ calisirsa android/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
